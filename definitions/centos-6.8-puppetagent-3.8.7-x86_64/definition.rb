8686ab9baa80cbfd4277f6bb8ebe3059c6e44c56
Javier Juarez <javier.juarez@gmail.com>
2016-10-10 13:47:22 +0200

    New definitions over the CentOS 6.8 base

---
+++ definitions/centos-6.8-puppetagent-3.8.7-x86_64/definition.rb
@@ -0,0 +1,36 @@
+Veewee::Session.declare({
+  :cpu_count => '1',
+  :memory_size=> '1024',
+  :disk_size => '10140',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :os_type_id => 'RedHat6_64',
+  :iso_file => "CentOS-6.8-x86_64-netinstall.iso",
+  :iso_src => "http://ftp.cica.es/CentOS/6.8/isos/x86_64/CentOS-6.8-x86_64-netinstall.iso",
+  :iso_md5 => "9e0ee4c5d9d0371fa0c0809b487ee5bb",
+  :iso_download_timeout => 1000,
+  :boot_wait => "10",
+  :boot_cmd_sequence => [
+    '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => 300,
+  :kickstart_file => "ks.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "vagrant",
+  :ssh_password => "vagrant",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
+  :postinstall_files => [
+    "base.sh",
+    "puppet.sh",
+    "vagrant.sh",
+    "virtualbox.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => 10000
+})
